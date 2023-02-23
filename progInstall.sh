1878c66b5823a05b3c376c667349b60325cb58fa
MacCygwin <mdyusufpool@gmail.com>
2023-02-23 17:08:44 +0800

    Create progInstall.sh

---
+++ progInstall.sh
@@ -0,0 +1,4 @@
+sudo apt install kwin --no-install-recommends 
+sudo apt install plank
+sudo apt install lxqt-panel
+sudo add-apt-repository ppa:agornostal/ulauncher && sudo apt update && sudo apt install ulauncher
